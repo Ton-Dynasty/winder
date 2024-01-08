85ad04f04727137d39052478894c417d21f1e462
grace0950 <pinyan0950@gmail.com>
2024-01-09 01:21:26 +0800

    Enhance: Improve data fetching speed with asyncio

--- market_price.py
+++ market_price.py
@@ -1,62 +1,47 @@
-import ccxt
+import ccxt.async_support as ccxt
 import numpy as np
 import time
+import asyncio
 
-# here is a list of exchanges that dont require KYC and have TON/USDT pair
 EXCHANGE_LIST = [
-    "bigone",
-    "bitfinex",
-    "bitfinex2",
-    "bitmart",
-    "bitopro",
-    "bitrue",
     "bybit",
-    "coinex",
-    "digifinex",
-    "exmo",
-    "gate",
     "gateio",
-    "htx",
-    "huobi",
-    "kucoin",
-    "lbank",
-    "mexc",
     "okx",
-    "poloniex",
 ]
 
 
-def fetch_ton_usdt_prices():
-    while True:  # infinite loop
-        prices = []
-        # time starts
+async def fetch_price_from_exchange(exchange_id, symbol="TON/USDT"):
+    try:
+        exchange_class = getattr(ccxt, exchange_id)
+        async with exchange_class() as exchange:
+            await exchange.load_markets()
+            if symbol in exchange.markets:
+                ticker = await exchange.fetch_ticker(symbol)
+                return ticker["last"]
+    except Exception as e:
+        print(f"Error fetching from {exchange_id}: {str(e)}")
+        return None
+
+
+async def fetch_ton_usdt_prices():
+    tasks = [fetch_price_from_exchange(exchange) for exchange in EXCHANGE_LIST]
+    prices = await asyncio.gather(*tasks)
+    return [price for price in prices if price is not None]
+
+
+def generator_ton_usdt_prices():
+    while True:
         start_time = time.time()
-        for exchange_id in EXCHANGE_LIST:
-            exchange_class = getattr(ccxt, exchange_id)
-            exchange = exchange_class()
-            try:
-                markets = exchange.load_markets()
-                if "TON/USDT" in markets:
-                    price = exchange.fetch_ticker("TON/USDT")["last"]
-                    prices.append(price)
-            except Exception as e:
-                print(f"Error fetching from {exchange_id}: {str(e)}")
-
-        # time ends
+        prices = asyncio.run(fetch_ton_usdt_prices())
         end_time = time.time()
-
+        print(f"Time taken: {end_time - start_time} seconds")
         if prices:
-            median_price = np.median(prices)
-            print(f"Time taken: {end_time - start_time} seconds")
-            yield median_price  # Use yield instead of return
+            yield np.mean(prices)
         else:
             yield None
 
 
 if __name__ == "__main__":
-    price_generator = fetch_ton_usdt_prices()
-    for current_median_price in price_generator:
-        if current_median_price is not None:
-            print(f"Current median price of TON/USDT: {current_median_price}")
-        else:
-            print("No prices fetched.")
+    prices = generator_ton_usdt_prices()
+    for _ in range(10):
+        print(next(prices))
