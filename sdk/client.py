3bb52620ce77069a575d6053be8f9ca819ce18b7
alan890104 <alan890104@gmail.com>
2024-01-28 12:03:42 +0800

    feat: Add OracleMetadata type and implement _get_oracle_metadata method

--- sdk/client.py
+++ sdk/client.py
@@ -4,31 +4,48 @@ import time
 from tonsdk.utils import Address
 from tonsdk.boc import Cell
 from tonsdk.contract.wallet import Wallets
-from typing import Dict, Tuple, Optional, Literal, Callable
+from typing import Dict, Tuple, Optional, Literal, Callable, TypedDict
 from arithmetic import FixedFloat
 from utils import to_token
 from os import getenv
 
+OracleMetadata = TypedDict(
+    "OracleMetadata",
+    {
+        "base_asset_address": str,
+        "quote_asset_address": str,
+        "base_asset_decimals": int,
+        "quote_asset_decimals": int,
+        "min_base_asset_threshold": int,
+        "base_asset_wallet_address": str,
+        "quote_asset_wallet_address": str,
+    },
+)
+
 
 class TicTonAsyncClient:
-    def __init__(
+    async def __init__(
         self,
         mnemonics: Optional[str] = None,
         oracle_addr: Optional[str] = None,
         toncenter_api_key: Optional[str] = None,
-        wallet_version: Optional[Literal[
-            "v2r1", "v2r2", "v3r1", "v3r2", "v4r1", "v4r2", "hv2"
-        ]] = None,
+        wallet_version: Optional[
+            Literal["v2r1", "v2r2", "v3r1", "v3r2", "v4r1", "v4r2", "hv2"]
+        ] = None,
         *,
         logger: Optional[logging.Logger] = None
     ) -> None:
         mnemonics = getenv("TICTON_WALLET_MNEMONICS", mnemonics)
         wallet_version = getenv("TICTON_WALLET_VERSION", wallet_version or "v4r2")
         oracle_addr = getenv("TICTON_ORACLE_ADDRESS", oracle_addr)
-        
-        assert mnemonics is not None, "mnemonics must be provided, you can either pass it as a parameter or set TICTON_WALLET_MNEMONICS environment variable"
-        assert oracle_addr is not None, "oracle_addr must be provided, you can either pass it as a parameter or set TICTON_ORACLE_ADDRESS environment variable"
-        
+
+        assert (
+            mnemonics is not None
+        ), "mnemonics must be provided, you can either pass it as a parameter or set TICTON_WALLET_MNEMONICS environment variable"
+        assert (
+            oracle_addr is not None
+        ), "oracle_addr must be provided, you can either pass it as a parameter or set TICTON_ORACLE_ADDRESS environment variable"
+
         _, _, _, self.wallet = Wallets.from_mnemonics(mnemonics.split(" "), wallet_version)  # type: ignore
         self.oracle = Address(oracle_addr)
         self.logger = logger or logging.getLogger(__name__)
@@ -36,10 +53,13 @@ class TicTonAsyncClient:
         # TODO: import toncenter client
         self.toncenter = None
 
-        # TODO: get these parameters from oracle
-        self.base_asset_decimals = 9
-        self.quote_asset_decimals = 6
-        self.min_base_asset_threshold = to_token(1, self.base_asset_decimals)
+        self.metadata = await self._get_oracle_metadata()
+
+    async def _get_oracle_metadata(self) -> OracleMetadata:
+        """
+        get the oracle's metadata
+        """
+        raise NotImplementedError
 
     async def _convert_price(self, price: float) -> FixedFloat:
         """
@@ -47,7 +67,9 @@ class TicTonAsyncClient:
         """
         assert price > 0, "price must be greater than 0"
         assert isinstance(price, float), "price must be a float"
-        return FixedFloat(price) * 10 ** (self.quote_asset_decimals - self.base_asset_decimals)
+        return FixedFloat(price) * 10 ** (
+            self.metadata["quote_asset_decimals"] - self.metadata["base_asset_decimals"]
+        )
 
     async def _get_user_balance(self) -> Tuple[Decimal, Decimal]:
         """
@@ -143,12 +165,12 @@ class TicTonAsyncClient:
             The new price of the position quoteAsset/baseAsset
         dry_run : bool
             Whether to call toncenter simulation api or not
-            
+
         Examples
         --------
         Assume the token pair is TON/USDT, the price is 2.5 USDT per TON. The position is opened with 1 TON and 2.5 USDT with index 123.
         The new price is 5 USDT per TON, the buy_num is 1.
-        
+
         >>> client = TicTonAsyncClient(...)
         >>> await client.wind(123, 1, 5)
         """
@@ -167,4 +189,3 @@ class TicTonAsyncClient:
         given callbacks.
         """
         raise NotImplementedError
-
